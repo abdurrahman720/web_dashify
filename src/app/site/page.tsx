3474340b135fa4e9b158628ea5010289af83631c
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-02-05 22:23:53 +0600

    landing page setup

---
+++ src/app/site/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="w-full h-full pt-36 relative flex items-center justify-center flex-col">
+        
+        <div className="absolute top-0 bottom-0 left-0 right-0
+        bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]
+        "/>
+         
+         <p className="text-center">Run your agency, in one place</p>
+          <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+            <h1 className="">
+           <span className="text-9xl font-bold text-center md:text-[300px]">w</span><span className="-ml-3 lg:-ml-10">eb</span>
+           <span className="text-9xl font-bold text-center md:text-[300px] ">D</span><span className="-ml-3 lg:-ml-10">ashi</span> <span className="text-9xl font-bold text-center md:text-[300px] -ml-3">fy</span>
+          </h1>
+        </div>
+        
+        
+
+    </section>
+    </>
+  );
+}
