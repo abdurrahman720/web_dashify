d8ff6cb87bc64cbe35fe77bcd6c5e5cf19e535f2
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-04-08 01:43:56 +0600

    added role based redirection in agency page

--- src/app/(main)/agency/(auth)/page.tsx
+++ src/app/(main)/agency/(auth)/page.tsx
@@ -1,22 +1,55 @@
+import AgencyDetails from "@/components/forms/agency-details";
 import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-const Page = async() => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) => {
+  const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
 
-   
+  //get user details and redirect based on their roles
+  const user = await getAuthUserDetails();
+  console.log(user);
 
-    const agencyId = await verifyAndAcceptInvitation()
-    console.log(agencyId)
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (searchParams?.plan) {
+        redirect(`/agency/${agencyId}/building?plan=${searchParams?.plan}`);
+      }
+      //if user is from stripe, state will contain details
+      if (searchParams?.state) {
+        const statePath = searchParams?.state?.split("__")[0];
+        const stateAgencyId = searchParams?.state?.split("___")[1];
+        if (!stateAgencyId) return <div>Not authorized</div>;
+        //redirect from stripe using the code
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams?.code}`
+        );
+      } else return redirect(`/agency/${agencyId}`);
+    } else {
+      return <div>Not authorized</div>;
+    }
+  }
 
-    const user = await getAuthUserDetails()
+  // if the don't have subaccount, they are not agency owner or admin and they are just new here
 
-    console.log(user)
+  const authUser = await currentUser();
 
-    
-
-
-  return <div>Agency website</div>;
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
+        <h1 className="text-4xl">Create an Agency</h1>
+        <AgencyDetails data={{companyEmail: authUser?.emailAddresses[0].emailAddress}} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/components/forms/agency-details.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { Agency } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
+import { AlertDialog } from "../ui/alert-dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type Props = {
+  data?: Partial<Agency>;
+};
+
+//this must be matched with the database model of agency
+const FormSchema = z.object({
+  name: z.string().min(2, { message: "Agency name must be atleast 2 chars." }),
+  companyEmail: z.string().min(1),
+  companyPhone: z.string().min(1),
+  whiteLabel: z.boolean(),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  state: z.string().min(1),
+  country: z.string().min(1),
+  agencyLogo: z.string().min(1),
+});
+
+const AgencyDetails = ({ data }: Props) => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [deletingAgency, setDeletingAgency] = useState(false);
+  const { handleSubmit, reset } = useForm<z.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: data?.name,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+      agencyLogo: data?.agencyLogo,
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      reset(data);
+    }
+  }, [data, reset]);
+
+  return (
+    <AlertDialog>
+      <Card className="w-full ">
+        <CardHeader>
+          <CardTitle>Agency Information</CardTitle>
+          <CardDescription>
+            Lets create an agency for you business. You can edit agency settings
+            later from the agency settings tab.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </AlertDialog>
+  );
+};
+
+export default AgencyDetails;
